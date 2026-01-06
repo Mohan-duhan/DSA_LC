83f8eef84a7708e83626582a376120944220af61
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2026-01-06 10:07:07 +0530

    Time: 2 ms (72.79%) | Memory: 109.6 MB (13.47%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,40 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> res;
-
-    void dfs(TreeNode* root, int level) {
-        if (root == NULL) return;
-
-        if (level == res.size()) {
-            res.push_back({});
-        }
-
-        res[level].push_back(root->val);
-
-        dfs(root->left, level + 1);
-        dfs(root->right, level + 1);
-    }
-
     int maxLevelSum(TreeNode* root) {
-        if (root == NULL) return 0;
+        if (root == nullptr)
+            return 0;
 
-        dfs(root, 0);
+        queue<TreeNode*> q;
+        q.push(root);
 
+        int lvl = 1;
         int maxSum = INT_MIN;
-        int minLevel = 1;
+        int anslvl = 1;
 
-        for (int i = 0; i < res.size(); i++) {
+        while (!q.empty()) {
+            int size = q.size();
             int sum = 0;
-            for (int j = 0; j < res[i].size(); j++) {
-                sum += res[i][j];
-            }
 
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                sum += node->val;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
             if (sum > maxSum) {
                 maxSum = sum;
-                minLevel = i + 1; // 1-based indexing
+                anslvl = lvl;
             }
+            lvl++;
         }
-
-        return minLevel;
+        return anslvl;
     }
 };
