589fd729f5dedf78aa1ec4469902d64b9de46e3a
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2026-01-11 17:16:54 +0530

    Time: 27 ms (78.23%) | Memory: 107.5 MB (73.57%) - LeetSync

---
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> mp;
+        int cnt = 0;
+
+        for (int t : tasks){
+            mp[t]++;
+        }
+
+        for(auto& it : mp){
+            if(it.second == 1)return -1;
+            else if(it.second % 3 == 0){
+                cnt += it.second/3;
+            }else{
+                cnt+=it.second/3 + 1;
+            }
+        }
+        return cnt;
+    }
+};
