89f5980ff534c71cd4d639e5b81789c41833c46b
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2026-01-15 18:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (78.29%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> leftMax(n, 0);
-        leftMax[0] = height[0];
-        vector<int> rightMax(n, 0);
-        rightMax[n - 1] = height[n - 1];
-
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = max(leftMax[i - 1], height[i - 1]);
-        }
-
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = max(rightMax[i + 1], height[i + 1]);
-        }
-
+    int trap(vector<int>& arr) {
+        int n = arr.size();
+        int left = 0;
+        int right = n - 1;
+        int leftMax = arr[left];
+        int rightMax = arr[right];
         int waterTrapped = 0;
-        for (int i = 0; i < n; i++) {
-            int currWater = min(leftMax[i], rightMax[i]) - height[i];
-            if (currWater > 0) {
-                waterTrapped += currWater;
+
+        while (left <= right) {
+            // case 1 : if left boundary is smaller
+            if (leftMax < rightMax) {
+                leftMax = max(leftMax, arr[left]);
+                waterTrapped += leftMax - arr[left];
+                left++;
+            }
+            // case 2 : if rightboundary is smaller
+            else {
+                rightMax = max(rightMax, arr[right]);
+                waterTrapped += rightMax - arr[right];
+                right--;
             }
         }
         return waterTrapped;
